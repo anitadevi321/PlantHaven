043b39ed734ca59bca069371b8e694677f968b7f
anitadevi321 <166592376+anitadevi321@users.noreply.github.com>
2024-05-30 11:52:11 +0530

    modified

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,33 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/shot-details', function () {
+    return view('shot-details');
+})->name('shot-details');
+
+Route::get('/cart', function(){
+    return view('cart');
+})->name('cart');
+
+Route::get('/checkout', function(){
+    return view('checkout');
+})->name('checkout');
